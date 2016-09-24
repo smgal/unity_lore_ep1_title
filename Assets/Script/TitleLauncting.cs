4227e210360425d08e4cd864ec79bbdea37eaab1
Youngki Ahn <avej.com@gmail.com>
2016-09-25 01:56:55 +0900

    initialize this project

---
+++ Assets/Script/TitleLauncting.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleLauncting : MonoBehaviour
+{
+	void OnInit()
+	{
+	}
+
+	void OnFloatingTitleEnd()
+	{
+		Animator animator = gameObject.GetComponent<Animator>();
+		animator.Stop();
+	}
+}
