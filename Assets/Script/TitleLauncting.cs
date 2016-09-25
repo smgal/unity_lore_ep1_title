33efc85009010e7c9cd19d13a6c2cb1b5bd4a07e
Youngki Ahn <avej.com@gmail.com>
2016-09-25 15:28:06 +0900

    Title animation in progress

--- Assets/Script/TitleLauncting.cs
+++ Assets/Script/TitleLauncting.cs
@@ -1,15 +1,44 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class TitleLauncting : MonoBehaviour
 {
-	void OnInit()
+	private GameObject m_text_bottom = null;
+	private GameObject m_dialog_box = null;
+	private GameObject m_title_scroll_bar = null;
+
+	void Start()
+	{
+		m_text_bottom = GameObject.FindGameObjectWithTag("TextBottom");
+		m_text_bottom.SetActive(false);
+
+		m_dialog_box = GameObject.FindGameObjectWithTag("ImageDialogBox");
+		m_dialog_box.SetActive(false);
+
+		m_title_scroll_bar = GameObject.FindGameObjectWithTag("TitleScrollBar");
+		m_title_scroll_bar.SetActive(false);
+	}
+
+	void OnFloatingTitle()
 	{
 	}
 
-	void OnFloatingTitleEnd()
+	void OnBottomText()
+	{
+		m_text_bottom.SetActive(true);
+	}
+
+	void OnDialogBox()
+	{
+		m_dialog_box.SetActive(true);
+	}
+
+	void OnTitleAnimationEnd()
 	{
 		Animator animator = gameObject.GetComponent<Animator>();
 		animator.Stop();
+
+		m_title_scroll_bar.SetActive(true);
 	}
 }
